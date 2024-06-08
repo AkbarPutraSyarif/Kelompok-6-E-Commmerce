d77542f9ac4e87e87b94584647413cf76915401c
GalangDining <dongdangding812@gmail.com>
2024-06-08 19:09:04 +0700

    Membuat Direct namun belum jadi

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,25 +3,37 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Product;
 
 class HomeController extends Controller
 {
-
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    // $products = Product::with('category')->get();
-
     public function index()
     {
-        $data = [
-            'title' => 'Dashboard',
-            'product' => Product::all()->count()
+        // Contoh data produk, bisa digantikan dengan data dari database
+        $products = [
+            ['id' => 1, 'name' => 'Indomie', 'harga' => 3000, 'image' => 'Barang1.jpg'],
+            ['id' => 2, 'name' => 'Teh Pucuk', 'harga' => 3500, 'image' => 'TehPucukHD.jpg'],
+            ['id' => 3, 'name' => 'Oreo', 'harga' => 7500, 'image' => 'Barang3.jpg'],
         ];
+        
 
-        return view('home', $data);
+        return view('home', compact('products'));
     }
+    public function purchase($id)
+    {
+        // Dapatkan detail produk berdasarkan ID
+        $products = [
+            ['id' => 1,'name' => 'Indomie', 'harga' => 3000, 'image' => 'Barang1.jpg'],
+            ['id' => 2,'name' => 'Teh Pucuk', 'harga' => 3500, 'image' => 'TehPucukHD.jpg'],
+            ['id' => 3,'name' => 'Oreo', 'harga' => 7500, 'image' => 'Barang3.jpg'],
+        ];
+    
+        if (!array_key_exists($id, $products)) {
+            abort(404);
+        }
+    
+        $product = $products[$id];
+    
+        return view('purchase', compact('product'));
+    }
+    
 }
