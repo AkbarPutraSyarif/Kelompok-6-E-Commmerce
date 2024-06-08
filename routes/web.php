55fe1fff2395caca578a8c1bd9c7fe3e69fb9ff7
GalangDining <dongdangding812@gmail.com>
2024-06-08 19:09:04 +0700

    Membuat Direct namun belum jadi

--- routes/web.php
+++ routes/web.php
@@ -4,20 +4,17 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\layout\controllerLogin;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('login');
-// });
-
-// Route::get('/registrasi', function () {
-//     return view('registrasi');
-// });
 
 Route::get('/',[controllerLogin::class,'login']);
 Route::post('/', [controllerLogin::class, 'loginPost']) ->name('login.post');
 Route::get('/registrasi',[controllerLogin::class,'register']);
 Route::post('/registrasi',[controllerLogin::class,'registerPost']);
+
 Route::get('/logout', [controllerLogin::class, 'logout']);
-Route::get('/beranda', [HomeController::class, 'index'])->name('home');
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+Route::get('/purchase/{id}', [HomeController::class, 'purchase'])->name('purchase');
+
 
 
 
