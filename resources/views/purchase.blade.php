fbe44fa289e94e1327a2f29f37fc2962e936395e
GalangDining <dongdangding812@gmail.com>
2024-06-27 19:27:31 +0700

    membuat database, model, controller, dan blade agar user bisa memberikan keluhannya kepada toko

--- resources/views/purchase.blade.php
+++ resources/views/purchase.blade.php
@@ -22,7 +22,7 @@
                     <a class="nav-link" href="{{ route('products.all') }}">Products</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Contact</a>
+                    <a class="nav-link" href="{{  route('contact') }}">Contact</a>
                 </li>
             </ul>
         </div>
